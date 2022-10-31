4ae685f36b45a6c5f8b971c4720a536dd521517e
Alex <a.matulionis@usa.com>
2022-10-30 21:06:01 -0700

    Improve color combinations

--- src/bgColors.ts
+++ src/bgColors.ts
@@ -1,16 +1,6 @@
 import { shuffle } from './utils'
 
-const bgColors = [
-  '#D03700',
-  '#E53D00',
-  '#F29300',
-  '#F9BE00',
-  '#90C550',
-  '#59B378',
-  '#21a0a0',
-  '#138483',
-  '#046865',
-] as const
+const bgColors = ['#D03700', '#F29300', '#F9BE00', '#90C550', '#59B378', '#21a0a0', '#046865'] as const
 
 function randomColorPair() {
   const shuffledBgColors = shuffle(bgColors)

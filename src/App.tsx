8bceba10f901a92ce44afeb7169ffc9b4d9e64a2
Alex <a.matulionis@usa.com>
2022-11-01 22:30:58 -0700

    Add QR code sharing capabilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import QRCode from 'qrcode'
 import { useEffect, useMemo, useRef, useState } from 'react'
+import toast from 'react-hot-toast'
 import useScroll from 'react-use/lib/useScroll'
 import useWindowSize from 'react-use/lib/useWindowSize'
 import { useMusic } from './music'
@@ -14,6 +16,19 @@ function App() {
   const [furthestVerseReached, setFurthestVerseReached] = useState(currentVerse)
   const nextFewVerses = furthestVerseReached + 3
 
+  const [showModal, setShowModal] = useState(false)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    if (!showModal) return
+
+    const canvas = canvasRef.current
+
+    QRCode.toCanvas(canvas, location.origin, err => {
+      if (err) toast.error(err.message)
+    })
+  }, [showModal])
+
   useEffect(() => {
     setFurthestVerseReached(max(currentVerse, furthestVerseReached))
   }, [currentVerse])
@@ -22,9 +37,34 @@ function App() {
   useMusic(randomSong)
 
   const elOverlay = (
-    <div className="fixed top-0 left-0 z-[999] w-full h-full pointer-events-none">
-      <div>currentVerse={currentVerse}</div>
-      <div>furthestVerseReached={furthestVerseReached}</div>
+    <div className="fixed top-0 left-0 z-[998] w-full h-full pointer-events-none p-10 flex justify-end items-end">
+      <div
+        className="text-2xl text-white opacity-50 cursor-pointer pointer-events-auto"
+        onClick={() => setShowModal(true)}
+      >
+        QR
+      </div>
+    </div>
+  )
+
+  const elModal = showModal && (
+    <div
+      className="fixed top-0 left-0 z-[999] w-full h-full flex justify-center items-center cursor-pointer bg-black/30"
+      onClick={() => setShowModal(false)}
+    >
+      <div
+        className="flex flex-col items-center gap-5 p-10 bg-white cursor-auto rounded-3xl"
+        onClick={e => e.stopPropagation()}
+      >
+        <div className="text-2xl">Share ElStory🕊</div>
+        <canvas
+          className="scale-150 mix-blend-multiply"
+          style={{
+            imageRendering: 'pixelated',
+          }}
+          ref={canvasRef}
+        />
+      </div>
     </div>
   )
 
@@ -45,7 +85,8 @@ function App() {
         ))}
       </div>
 
-      {/* {elOverlay} */}
+      {elOverlay}
+      {elModal}
     </>
   )
 }

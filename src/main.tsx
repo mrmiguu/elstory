6f30b8f974fd89c01c5dc80d5a98effd0891d28c
Alex <a.matulionis@usa.com>
2024-01-20 09:30:27 -0800

    Add foundation for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import { MainProvider } from './main.context.tsx'
+import { router } from './routes.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MainProvider>
-      <App />
+      <RouterProvider router={router} />
     </MainProvider>
     <Toaster />
   </React.StrictMode>,

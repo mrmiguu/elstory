1f814ba4adb61f5c0d0b61eee771f243753ffd2f
Alex <a.matulionis@usa.com>
2024-01-20 22:23:01 -0800

    Add base Chapter collection route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from './App'
+import ChapterCollection from './ChapterCollection'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        path: 'chapters',
+        element: <ChapterCollection />,
+      },
+    ],
   },
 ])
